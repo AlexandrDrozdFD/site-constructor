31ba83a799070c3b688948ffe93df8ca9640584c
Alexandr Drozd <alex.drozd.fd@gmail.com>
2021-06-25 17:44:10 +0300

    feat: add Sidebar class

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import {model} from './model.js';
 import {Site} from './classes/site.js';
 import './styles/main.css';
+import {Sidebar} from "./classes/sidebar.js";
 
 const site = new Site('#site');
 
 site.render(model);
+
+const updateCallback = (newBlock) => {
+  model.push(newBlock);
+  site.render(model);
+}
+
+new Sidebar('#panel', updateCallback);
 
 

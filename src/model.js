a8452967f2ee8e205e3f0539010b743ac0208612
Alexandr Drozd <alex.drozd.fd@gmail.com>
2021-06-25 13:10:37 +0300

    feat: add extended classes to each block in model

--- src/model.js
+++ src/model.js
@@ -1,12 +1,12 @@
 import image from './assets/image.png';
-import Block from './classes/blocks.js';
+import {TitleBlock, ImageBlock, TextBlock, ColumnsBlock} from './classes/blocks.js';
 
 const text = `
   Another code you can see here: <a href="https://github.com/AlexandrDrozdFD" target="_blank">GitHub</a>
 `
 
 export const model = [
-  new Block('title', 'Site Constructor with pure JavaScript', {
+  new TitleBlock('Site Constructor with pure JavaScript', {
     tag: 'h2',
     styles: {
       background: 'linear-gradient(to right, #ff0099, #493240)',
@@ -16,7 +16,7 @@ export const model = [
     }
   }),
 
-  new Block('image', image, {
+  new ImageBlock(image, {
     styles: {
       padding: "2rem 0",
       display: 'flex',
@@ -28,7 +28,7 @@ export const model = [
     },
     alt: 'Random picture'
   }),
-  new Block('text', text, {
+  new TextBlock(text, {
     styles: {
       background: 'linear-gradient(to left, #f2994a, #f2c94c)',
       padding: '1rem',
@@ -36,8 +36,7 @@ export const model = [
     }
   }),
 
-  new Block(
-    'columns',
+  new ColumnsBlock(
     [
       'This application is made with pure JS without any frameworks',
       'Also you will learn SOLID principles and OOP',
